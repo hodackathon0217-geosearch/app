5b8b4bb84c142a70ea61bbeb1d5ec3b2ca7c06bf
Stephen Scott <s.p.scott@kainos.com>
2017-02-28 11:57:22 +0000

    Seeding data

--- seed/seed.js
+++ seed/seed.js
@@ -3,9 +3,26 @@
 const request = require('request-promise');
 const exampleDocuments = require('./convertcsv.json');
 const ELASTICSEARCH_URL = process.env.ELASTICSEARCH_URL;
-const AdmZip = require('adm-zip');
+const extract = require('extract-zip')
+const fs = require('fs');
+const parse = require('csv-parse');
 
-const zip = new AdmZip("./data/crimeDataAggregated.csv.zip");
+function readCSV() {
+	return new Promise((resolve, reject) => {
+		extract('/app/seed/data/crimeDataAggregated.csv.zip', {dir: '/app/seed/data/'}, function (err) {
+			console.log('Creating CSV', err)
+			fs.createReadStream('/app/seed/data/crimeDataAggregated.csv')
+          .pipe(parse({delimiter: ','}))
+          .on('data', function(csvrow) {
+             createData(csvrow)
+          })
+          .on('end',function() {
+            resolve();
+          });
+		})
+	})
+}
+
 
 function deleteIndex() {
   console.log('Deleting index (and data)...');
@@ -19,18 +36,28 @@ function deleteIndex() {
 }
 
 function createData(row) {
-  console.log('Creating data...' , row['Crime ID']);
-  row.location = {
-    lat : row.Latitude,
-    lon : row.Longitude,
+  console.log('Creating data...' , row[0]);
+  const record = {
+  	Month: row[0],
+  	"Reported by": row[3],
+  	"Falls within": row[4],
+  	"Location": row[5],
+  	"LSOA code": row[6],
+  	"LSOA name" : row[7],
+  	"Crime type": row[8],
+  	"Last outcome category":  row[9]
+  }
+  const lat = row[2] === 'NA' ? 0 : parseFloat(row[2])
+  const lon = row[1] === 'NA' ? 0 : parseFloat(row[1])
+  record.location = {
+    lat : lat,
+    lon : lon,
   };
-  delete row.Latitude;
-  delete row.Longitude;
   const opts = {
     method: 'POST',
     uri: `${ELASTICSEARCH_URL}/test/thetype`,
     json: true,
-    body: row,
+    body: record,
   };
   return request(opts);
 }
@@ -55,10 +82,8 @@ function createMapping() {
   };
   return request(opts);
 }
-
 deleteIndex()
 	.then(createMapping)
-	.then(() => zip.extractAllTo('./data/crimeData', true);)
-	.then(() => Promise.all(exampleDocuments.map(createData)))
+	.then(readCSV)
   .then(() => console.log('Seeding done!'))
   .catch(err => console.log('ERROR!!', err));
